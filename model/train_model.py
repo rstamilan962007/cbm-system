4fdc58a053bb2e9abcde47c85f1487106b894a90
rstamilan962007 <rstamilan962007@gmail.com>
2026-06-21 11:43:18 +0530

    Implement model training with Decision Tree Classifier

--- model/train_model.py
+++ model/train_model.py
@@ -1,1 +1,14 @@
-print("Model Training Started")
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv('../dataset/machine_data.csv')
+
+X = data[['Temperature','Vibration','RunningHours']]
+y = data['Status']
+
+model = DecisionTreeClassifier()
+model.fit(X,y)
+
+prediction = model.predict([[75,0.9,500]])
+
+print("Predicted Status:", prediction[0])
